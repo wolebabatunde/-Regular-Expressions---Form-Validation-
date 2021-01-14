825ce8119e01f9c064d4a61659f50bac9bf36e15
wolebabatunde <kolawole_yekini@yahoo.com>
2021-01-14 20:40:30 +0100

    UI

---
+++ app.js
@@ -0,0 +1,32 @@
+// FORM BLUR EBENT LISTENERS
+document.getElementById('name').addEventListener('blur',
+    validateName)
+
+document.getElementById('zip').addEventListener('blur',
+    validateZip)
+
+document.getElementById('email').addEventListener('blur',
+    validateEmail)
+
+document.getElementById('phone').addEventListener('blur',
+    validatePhone)
+
+// VALIDATE NAME FUNCTION
+function validateName() {
+
+}
+
+// VALIDATE ZIP FUNCTION
+function validateZip() {
+
+}
+
+// VALIDATE EMAIL FUNCTION
+function validateEmail() {
+
+}
+
+// VALIDATE PHONE FUNCTION
+function validatePhone() {
+
+}
