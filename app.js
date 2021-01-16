06ec7aa35ace946cf885857cca19f615629fee54
github_wolebabatunde <kolawole_yekini@yahoo.com>
2021-01-16 11:03:53 +0100

    reg

--- app.js
+++ app.js
@@ -13,12 +13,26 @@ document.getElementById('phone').addEventListener('blur',
 
 // VALIDATE NAME FUNCTION
 function validateName() {
+    const name = document.getElementById('name');
+    const re = /^[a-zA-Z]{2,10}$/;
 
+    if (!re.test(name.value)) {
+        name.classList.add('is-invalid');
+    } else {
+        name.classList.remove('is-invalid');
+    }
 }
 
 // VALIDATE ZIP FUNCTION
 function validateZip() {
+    const zip = document.getElementById('zip');
+    const re = /^[0-9]{5}(-[0-9]{4})?$/
 
+    if (!re.test(zip.value)) {
+        zip.classList.add("is-invalid");
+    } else {
+        zip.classList.remove("is-invalid")
+    }
 }
 
 // VALIDATE EMAIL FUNCTION
